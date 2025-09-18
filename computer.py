21c36f370d417c216461eeb8a4616fc2462b54db
kmccollum-star <kmccollum@smith.edu>
2025-09-18 16:10:36 -0400

    First Draft

--- computer.py
+++ computer.py
@@ -1,10 +1,61 @@
+# class Computer initializes and store information about computers
 class Computer:
 
-    # What attributes will it need?
+    description: str
+    processor: str
+    hardDrive: int
+    memory: int
+    operatingSystem:str
+    year_made: int
+    price: int
+    
+    # constructor method initializes parameters for computer
+    def __init__(self, description: str,
+                    processor_type: str,
+                    hard_drive_capacity: int,
+                    memory: int,
+                    operating_system: str,
+                    year_made: int,
+                    price: int):
+        self.description = description
+        self.year_made = year_made
+        self.processor = processor_type
+        self.hard_drive = hard_drive_capacity
+        self.operating_system = operating_system
+        self.price = price
+        self.memory = memory
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    # updates the operating system from an older one to the new one
+    def update_OS(self, computer_1):
+        new_OS = "MacOS Monterey"
+        print("Updating OS", computer_1.operating_system, ", to OS ", new_OS) 
+        print("Done.\n")
+    
+    # updates the price of the computer depending on the year made
+    def update_price(self, computer_1):
+       new_price = computer_1.price
+       if int(computer_1.year_made) < 2000:
+                computer_1.price = 0 # too old to sell, donation only
+       elif int(computer_1.year_made) < 2012:
+                computer_1.price = 250 # heavily-discounted price on machines 10+ years old
+       elif int(computer_1.year_made) < 2018:
+                computer_1.price= 550 # discounted price on machines 4-to-10 year old machines
+       else:
+                computer_1.price = 1000 # recent stuff
+            
+       new_price = computer_1.price
+       print("new price: ", new_price)
 
-    # What methods will you need?
+# computer information entered and printed
+def main():
+        computer_1 = Computer("Mac Pro (Late 2013)",
+        "3.5 GHc 6-Core Intel Xeon E5",
+        1024, 
+        64,
+        "macOS Big Sur", 
+        2013, 
+        1500)
+        print(computer_1.description)
+
+if __name__ == "__main__":
+    main()
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,63 @@
+from typing import Dict
+from computer import Computer
+
+# contains the methods that allow the resale shop to buy, sell, refurbish, and store inventory
 class ResaleShop:
 
-    # What attributes will it need?
+    inventory : list
+    new_price = int
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    
+    def __init__(self):
+        self.inventory = [] 
 
-    # What methods will you need?
+# updates the computer's OS and price in one method and prints error if inventory is empty
+    def refurbish(self, computer_1: Computer):
+        if computer_1 in self.inventory:
+            print("Refurbishing: ", computer_1.description)
+            computer_1.update_OS(computer_1) 
+            computer_1.update_price(computer_1)
+        else: 
+            print("Error.")
+
+
+# checks that inventory has items in it and prints the description of these items
+    def print_inventory(self, computer_1: Computer):
+        if len(self.inventory) != 0 :
+            for self.item in self.inventory:
+                print(f"Item Description: - {(self.item.description)}")
+        else:
+            print("No inventory to display.")
+    
+# buys computer and adds to resale shop inventory
+    def buy(self, computer_1: Computer):
+        self.inventory.append(computer_1)
+        print("Buying", computer_1.description)
+        print("Adding to inventory...")
+        print("Done.\n")
+        return self.inventory.index(computer_1)
+
+
+# sells computer and removes from resale shop inventory
+    def sell(self, computer_1: Computer):
+        print("Selling Item:", computer_1.description)
+        self.inventory.remove(computer_1)
+
+# stores computer class and resale shop class and runs different methods
+def main():
+    mystore = ResaleShop()
+    computer_1 = Computer("Mac Pro (Late 2013)",
+        "3.5 GHc 6-Core Intel Xeon E5",
+        1024, 
+        64,
+        "macOS Big Sur", 
+        2013, 
+        1500)
+    mystore.print_inventory(computer_1)
+    mystore.buy(computer_1)
+    mystore.sell(computer_1)
+    mystore.refurbish(computer_1)
+    mystore.print_inventory(computer_1)
+    
+
+main()
